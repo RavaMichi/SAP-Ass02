7b464f5b4feaf4c29e625b03c9ab12bbfac5a5c1
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2024-11-27 13:54:03 +0100

    implement methods to AccountManagerLogic

--- AccountManager/src/main/java/service/application/AccountManagerLogic.java
+++ AccountManager/src/main/java/service/application/AccountManagerLogic.java
@@ -23,27 +23,37 @@ public class AccountManagerLogic implements AccountManager{
 
     @Override
     public Optional<User> getUser(String username) {
-        return Optional.empty();
+        return accountDatabase.get(username);
     }
 
     @Override
     public List<User> getAllUsers() {
-        return List.of();
+        return accountDatabase.getAll();
     }
 
     @Override
     public void addUser(String username) throws AccountOperationException {
-
+        if (getUser(username).isPresent()) {
+            throw new AccountOperationException("User " + username + " already exists");
+        } else {
+            User newUser = new User(username, 0);
+            accountDatabase.add(newUser);
+            this.listeners.forEach(l -> l.onUserAdded(newUser));
+        }
     }
 
     @Override
     public void addCreditToUser(String username, int amount) throws AccountOperationException {
-
+        User u = getUser(username).orElseThrow(() -> new AccountOperationException("User " + username + " does not exist"));
+        int prevCredits = u.getCredits();
+        int newCredits = amount + prevCredits;
+        accountDatabase.updateCredit(u, newCredits);
+        this.listeners.forEach(l -> l.onUserCreditSet(u, prevCredits, newCredits));
     }
 
     @Override
     public void removeCreditFromUser(String username, int amount) throws AccountOperationException {
-
+        addCreditToUser(username, -amount);
     }
 
     @Override
