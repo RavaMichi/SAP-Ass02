0bc0c471c8d33f3534c8d676ab5102d2e0c2c519
Michele Ravaioli <michele.ravaioli3@studio.unibo.it>
2024-12-02 19:30:04 +0100

    Update Config.java

--- BikeManager/src/main/java/service/infrastructure/db/Config.java
+++ BikeManager/src/main/java/service/infrastructure/db/Config.java
@@ -13,13 +13,19 @@ import service.infrastructure.auth.AuthChecker;
 public class Config {
     public static String databasePath = "db/.bikes";
 
-    private AuthChecker authChecker;
-    private FileBikeDatabase fileBikeDatabase;
+    private final AuthChecker authChecker;
+    private final FileBikeDatabase fileBikeDatabase;
+
+    public Config(AuthChecker authChecker, FileBikeDatabase fileBikeDatabase) {
+        this.authChecker = authChecker;
+        this.fileBikeDatabase = fileBikeDatabase;
+    }
 
     @Post("/set-db-path")
     public HttpResponse<String> setDbPath(@Header(HttpHeaders.AUTHORIZATION) String token, @Body String dbPath) {
         if (authChecker.hasAdminPermissions(token)) {
             databasePath = dbPath;
+            fileBikeDatabase.init(databasePath);
             return HttpResponse.ok(databasePath);
         } else {
             return HttpResponse.unauthorized();
